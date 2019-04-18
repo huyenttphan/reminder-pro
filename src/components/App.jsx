e515b2a8c4a708958dae21dfc4bfb37ea331fa00
Huyen Phan <hphan@aml0jmhur.arbormetrix.local>
2019-04-18 16:19:15 -0400

    first push

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+  }
+
+  addReminder() {
+    console.log("this.state", this.state);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="title">Reminder Pro</div>
+        <div className="form-inline">
+          <div className="form-group">
+            <input
+              className="form-control"
+              placeholder="I have to..."
+              onChange={event => this.setState({ text: event.target.value })}
+            />
+            <button
+              type="button"
+              className="btn btn-success"
+              onClick={() => this.addReminder()}
+            >
+              Add reminder
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
